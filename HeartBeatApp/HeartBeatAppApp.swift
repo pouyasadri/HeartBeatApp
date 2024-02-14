82082ffc19b1af5199e380d1bbec83dd2ef13630
pouyasadri <pouyapaghandeh78@gmail.com>
2024-02-14 20:31:16 +0100

    Initial Commit

---
+++ HeartBeatApp/HeartBeatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeartBeatAppApp.swift
+//  HeartBeatApp
+//
+//  Created by Pouya Sadri on 14/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct HeartBeatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
